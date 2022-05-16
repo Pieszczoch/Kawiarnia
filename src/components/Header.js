3d5ab20039c64035e928bcf7b03d1d296fc15246
Sebastian Kopacki <sebastian.kopacki5@gmail.com>
2022-05-16 20:26:02 +0200

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ function Header() {
 }
 
 const MainContainer = styled.header`
- background: url(../images/coffee-g741ef407d_1920.jpg) no-repeat center/cover;
+ background: url(../../background/coffee-g741ef407d_1920.jpg) no-repeat center/cover;
  height: 35rem;
  -webkit-box-shadow: inset 0px 0px 100px 100px rgba(0, 0, 0, 1);
  -moz-box-shadow: inset 0px 0px 100px 100px rgba(0, 0, 0, 1);
